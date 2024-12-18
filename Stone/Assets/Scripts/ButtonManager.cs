204d7e6575c79378e6e9b3a24fbde32a722463d4
taigadayoo <jpapaxx@gmail.com>
2024-12-18 18:02:33 +0900

    BGM追加

--- Stone/Assets/Scripts/ButtonManager.cs
+++ Stone/Assets/Scripts/ButtonManager.cs
@@ -7,7 +7,7 @@ public class ButtonManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        SoundManager.Instance.PlayBgm(BgmType.BGM1);
     }
 
     // Update is called once per frame
@@ -17,18 +17,22 @@ public class ButtonManager : MonoBehaviour
     }
     public void OnTitle()
     {
+        SoundManager.Instance.StopBgm();
         SceneManagement.Instance.OnTitle();
     }
     public void OnGame()
     {
+        SoundManager.Instance.StopBgm();
         SceneManagement.Instance.OnGame();
     }
     public void OnChallenge()
     {
+        SoundManager.Instance.StopBgm();
         SceneManagement.Instance.OnChallenge();
     }
     public void OnBattle()
     {
+        SoundManager.Instance.StopBgm();
         SceneManagement.Instance.OnBattle();
     }
 }
